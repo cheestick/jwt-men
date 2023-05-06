e8703a39227aead55dc094c8e5c0f5211ddfdae6
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2023-05-06 21:32:23 +0300

    tokenezation was added

--- controllers/main.js
+++ controllers/main.js
@@ -1,14 +1,50 @@
+const CustomAPIError = require("../errors/custom-error");
+const jwt = require("jsonwebtoken");
+
 const login = async (req, res) => {
-  res.send("Fake Login/Register/Singup Route");
+  const { username, password } = req.body;
+
+  // mongo
+  // Joi
+  // check in the controller
+
+  if (!username || !password) {
+    throw new CustomAPIError("Please provide emain and password", 400);
+  }
+
+  // just for demo, normally provided bt DB!
+  const id = new Date().getDate();
+
+  // try to keep payload small, better experience for user
+  // secret - just for demo, in production use long, complex and unguessable string value!
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+
+  res.status(200).json({ msg: "user created", token });
 };
 
 const dashboard = async (req, res) => {
-  const luckyNumber = Math.floor(Math.random() * 100);
+  const authHeader = req.headers.authorization;
 
-  res.status(200).json({
-    msg: `Hello, John Doe`,
-    secret: `Here is your authorized data, you lucky number is ${luckyNumber}`,
-  });
+  if (!authHeader || !authHeader.startsWith("Bearer")) {
+    throw new CustomAPIError("No token provided", 401);
+  }
+
+  const [, token] = authHeader.split(" ");
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const luckyNumber = Math.floor(Math.random() * 100);
+
+    res.status(200).json({
+      msg: `Hello, ${decoded.username}`,
+      secret: `Here is your authorized data, you lucky number is ${luckyNumber}`,
+    });
+  } catch (error) {
+    throw new CustomAPIError("Not authorized to access this route", 401);
+  }
 };
 
 module.exports = {
